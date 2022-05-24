bb9df4a6bc4c4a7d7ff04dc0f6d0c91e8676e96a
wandaarvaniyan <72059528+WandaArvaniyan@users.noreply.github.com>
2022-05-24 16:07:01 +0700

    update

--- application/views/themes/footer.php
+++ application/views/themes/footer.php
@@ -1,10 +1,8 @@
-        <script src="<?php echo base_url()?>https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url()?>js/scripts.js"></script>
-        <script src="<?php echo base_url()?>https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url()?>assets/demo/chart-area-demo.js"></script>
-        <script src="<?php echo base_url()?>assets/demo/chart-bar-demo.js"></script>
-        <script src="<?php echo base_url()?>https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url()?>js/datatables-simple-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url()?>assets/js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script> 
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url()?>assets/js/datatables-simple-demo.js"></script>
 
         </div>
           <!-- /.row -->
