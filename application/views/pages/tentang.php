0e6fc59f2c8a0ec27e5e1ef02b63fe89969030d8
wandaarvaniyan <72059528+WandaArvaniyan@users.noreply.github.com>
2022-05-25 22:54:45 +0700

    update tentang.php

--- application/views/pages/tentang.php
+++ application/views/pages/tentang.php
@@ -1,6 +1,5 @@
-<link href="assets/css/styles.css" rel="stylesheet" />
 <h2>Tentang Kami</h2>
-<!-- <img src="assets/images/abaya_outer.jpg" width="300" height="300" style="float:left" /> -->
+<img class="width(100) height(100)" src="assets/images/abaya_outer.jpg" style="float:left" />
 <strong> Deskripsi</strong><br>
 <p>OPEN <br>
 Senin - Sabtu <br>
@@ -8,5 +7,10 @@ Jam Kerja 09.00-17.00<br>
 Minggu TIDAK ADA PENGIRIMAN<br>
 Transfer max jam 16.00 selebihnya ikut pengiriman besok<br>
 Happy Shopping Maebabies ❤</p> <br>
+<h5>Sosial Media</h5>
+<p> 
+    <a color="black" href="https://www.instagram.com/maezula/"> <i class="fa-brands fa-instagram"></i><color="black"> Instagram </a><br>
+    <a href="https://www.tiktok.com/@maezula.co?lang=id-ID"> <i class="fa-brands fa-instagram"></i> Tiktok </a><br>
+    <a href="https://wa.me/6811532776350/"> <i class="fa-brands fa-instagram"></i> WhatsApp </a>
 
 
